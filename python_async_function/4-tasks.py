6ca928fbe59df2e0a940f5cbce43cf2cc67f0579
waelbessaies <waelbessaies2@gmail.com>
2023-09-28 16:50:29 +0100

    Tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""task"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """returns list"""
+    tasks = []
+    result_list = []
+    for i in range(n):
+        tasks.append(task_wait_random(max_delay))
+
+    result_list = [await task for task in asyncio.as_completed(tasks)]
+    return result_list
