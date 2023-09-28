8583b30d30afbc0f2088c604e7bca812e10017e4
waelbessaies <waelbessaies2@gmail.com>
2023-09-28 16:55:12 +0100

    Tasks

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""task"""
+"""Take the code from wait_n and alter it into a new function task_wait_n. The code is nearly identical to wait_n except task_wait_random is being called.
+
+"""
 import asyncio
 from typing import List
 
