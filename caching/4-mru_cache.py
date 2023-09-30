4d42e4b30998f614e27ecd5fbd2e9c19a169e276
waelbessaies <waelbessaies2@gmail.com>
2023-09-30 11:16:19 +0100

    mrucaching

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+MRU Caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Class that inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.head, self.tail = '-', '='
+        self.next, self.prev = {}, {}
+        self.handle(self.head, self.tail)
+
+    def handle(self, head, tail):
+        self.next[head], self.prev[tail] = tail, head
+
+    def _remove(self, key):
+        self.handle(self.prev[key], self.next[key])
+        del self.prev[key], self.next[key], self.cache_data[key]
+
+    def _add(self, key, item):
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.prev[self.tail]))
+            self._remove(self.prev[self.tail])
+        self.cache_data[key] = item
+        self.handle(self.prev[self.tail], key)
+        self.handle(key, self.tail)
+
+    def put(self, key, item):
+        if key and item:
+            if key in self.cache_data:
+                self._remove(key)
+            self._add(key, item)
+
+    def get(self, key):
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            self._remove(key)
+            self._add(key, value)
+            return value
