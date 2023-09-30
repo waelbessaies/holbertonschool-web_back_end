4fb1fef02e4a17ee6d53a8b0554017a67b912800
waelbessaies <waelbessaies2@gmail.com>
2023-09-30 11:15:35 +0100

    lifocache

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+LIFO caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFO Cache
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            self.cache_data.pop(self.remove)
+            print("DISCARD: {}".format(self.remove, end=""))
+        if key:
+            self.remove = key
+        else:
+            pass
+
+    def get(self, key):
+        if key is None:
+            return None
+        return self.cache_data.get(key)
