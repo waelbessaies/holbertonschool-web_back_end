9a45163a40c31b13763869eadb41d86e0d7a28ca
waelbessaies <waelbessaies2@gmail.com>
2023-10-15 15:00:21 +0100

    user.py

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-"""user module"""
-from sqlalchemy import Column, Integer, String
+""" user module """
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    """class User"""
+    """ class comment """
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
